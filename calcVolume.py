bf56119442247dc296b82a5bb36cfa0ed94993da
matheus claubert <mattheus_claubert@hotmail.com>
2025-05-21 19:34:44 -0300

    reorganização do estudo

--- calcVolume.py
+++
@@ -1,9 +0,0 @@
-#calculadora de volume sem def
-
-# Definindo as variáveis
-raio = float(input("Digite o raio da esfera: "))
-pi = 3.14
-# Calculando o volume da esfera
-volume_esfera = (4/3) * pi * (raio ** 3)
-# Exibindo o resultado
-print("O volume da esfera é:", volume_esfera)
